c27311411aa8c0095d6d8acbf3efac2e49063e57
Prabal864 <940pps@gmail.com>
2024-11-26 15:20:59 +0530

    final dep

--- backend/app.js
+++ backend/app.js
@@ -10,11 +10,18 @@ import connectToMongoDB from "./db/mongoDBConnection.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const __dirname2 = path.resolve();
 dotenv.config({ path: path.resolve(__dirname, ".env") });
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use("/api/v1",appRouter);
+
+app.use(express.static(path.join(__dirname2,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname2,"/frontend","dist","index.html"))
+})
 
 const port = process.env.PORT || 5000;
 
