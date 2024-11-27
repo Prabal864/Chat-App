9bc9bd7ac7666cfdd13452a6896d2bfd7b9b596a
Prabal864 <940pps@gmail.com>
2024-11-27 12:55:56 +0530

    link

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chat-app-v6cv.onrender.com",{
+			const socket = io("https://microservice-chat-app.onrender.com",{
                query:{
                 userId: authUser._id
                } 
